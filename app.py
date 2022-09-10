fc6b68404ccc03e69825140b6ddffe4828cd700f
a26796879 <33983387+a26796879@users.noreply.github.com>
2022-09-11 01:58:09 +0800

    Add route for homepage & apis

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 """ SongGuess Backend """
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
+from main import SongGuess
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('learn.html')
+    """ homepage """
+    return render_template('index.html')
 
 
-@app.route('/question')
-def download_file():
-    return send_from_directory(r'music/', 'question.wav')
+@app.route('/get_quiz/<playlist_id>')
+def get_quiz(playlist_id):
+    """ 取得歌單 """
+    return SongGuess.get_playlist(playlist_id)
+
+
+@app.route('/get_preview/<song_id>')
+def get_preview(song_id):
+    """ 取得試聽連結 """
+    return {"preview_url": SongGuess.get_preview(song_id)}
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
