715be47801acb01c283012ae966dc47ddc942bc2
a26796879 <33983387+a26796879@users.noreply.github.com>
2022-09-11 01:56:45 +0800

    Change functions

--- lyrics.py
+++ lyrics.py
@@ -20,7 +20,7 @@ class MusixMatch:
     def get_track(cls, singer, track_name):
         """ 根據歌手&歌名 取得歌詞ID """
         track_url = f'{URL}matcher.track.get?apikey={API_KEY}&q_artist={cls.TO_SAMPLE.convert(singer)}&q_track={track_name}'
-        print(track_url)
+        #　print(track_url)
         res = requests.get(url=track_url, headers=HEADERS)
         res_status_code = res.json()['message']['header']['status_code']
         if res_status_code != 200:
@@ -41,16 +41,8 @@ class MusixMatch:
     @staticmethod
     def speech_lyric(lyric):
         """ 念出歌詞 """
-        text_speech = pyttsx3.init()
+        # text_speech = pyttsx3.init()
         # text_speech.say(lyric)
-        text_speech.save_to_file(lyric, 'question.mp3')
-        text_speech.runAndWait()
+        # text_speech.save_to_file(lyric, 'question.mp3')
+        # text_speech.runAndWait()
         return True
-
-
-if __name__ == '__main__':
-    track = 1  # MusixMatch.get_track(singer=SINGER, track_name=TRACK)
-    print(track)
-# lyrics = MusixMatch.get_lyrics(track).replace(
-#     '******* This Lyrics is NOT for Commercial use *******', '').replace('(1409622762958)', '')
-# print(lyrics)
